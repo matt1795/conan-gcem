b8f4aca95733ebf77e51e440939c0b6b85ceca82
Matthew Knight <mgk1795@gmail.com>
2019-08-10 19:17:18 +0000

    added needed gcem stuff

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,9 +1,16 @@
-from conans import ConanFile, tools
-import os
+from conans import ConanFile, tools, CMake
 
 
 class TestPackageConan(ConanFile):
+    requires = "gcem/1.12.0@matt1795/stable"
+    generators = "cmake_find_package"
+    exports_sources = "*"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder=self.source_folder)
+        cmake.build()
 
     def test(self):
         if not tools.cross_building(self.settings):
-            self.run("some_tool --version")
+            self.run("./test_package")
---
+++ test_package/main.cpp
@@ -0,0 +1,5 @@
+#include "gcem.hpp"
+
+int main() {
+
+}
