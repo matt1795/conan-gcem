4a1759bae54ee881a91352bd5451d36f91aed8cc
Matthew Knight <mgk1795@gmail.com>
2019-08-10 18:12:27 +0000

    Initial commit

---
+++ test_package/conanfile.py
@@ -0,0 +1,9 @@
+from conans import ConanFile, tools
+import os
+
+
+class TestPackageConan(ConanFile):
+
+    def test(self):
+        if not tools.cross_building(self.settings):
+            self.run("some_tool --version")
